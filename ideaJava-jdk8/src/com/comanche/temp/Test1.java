fa9be1dae1d2c2fa604c2fb5dc381f7e58a73b1c
antsgod <antsgod30329398@163.com>
2021-03-02 00:17:11 +0800

    合并hot_fix分支代码

--- ideaJava-jdk8/src/com/comanche/temp/Test1.java
+++ ideaJava-jdk8/src/com/comanche/temp/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("测试");
         System.out.println("host_fix分支修改");
+        System.out.println("master-测试");
     }
 }
