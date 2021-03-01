2ae6ea18aedb45edd693110abbbb05f58b5103f2
antsgod <antsgod30329398@163.com>
2021-03-02 00:18:15 +0800

    hot_fix代码

---
+++ ideaJava-jdk8/src/com/comanche/temp/Test2.java
@@ -0,0 +1,8 @@
+package com.comanche.temp;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+        System.out.println("host_fix分支修改");
+    }
+}
