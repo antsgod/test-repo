15f6a8ec414544da3c0f3935b9f15b8879ca3a81
antsgod <antsgod30329398@163.com>
2021-03-01 23:27:40 +0800

    第二次提交

--- ideaJava-jdk8/src/com/comanche/MultiThread/chapter05/section01/TimerTest1.java
+++ ideaJava-jdk8/src/com/comanche/MultiThread/chapter05/section01/TimerTest1.java
@@ -12,20 +12,7 @@ import java.util.TimerTask;
  */
 public class TimerTest1 {
     public static void main(String[] args) {
-        Timer timer = new Timer();
-        LocalDateTime now = LocalDateTime.now();
-        System.out.println(now);
-        Instant instant = now.atZone(ZoneId.systemDefault()).toInstant();
-        Date date = Date.from(instant);
-        timer.schedule(new TimerTask1(),date,5000);
-        for (int i = 0; i < 50; i++) {
-            try {
-                System.out.println(i);
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        System.out.println("测试");
     }
 }
 
--- ideaJava-jdk8/src/com/comanche/temp/test.java
+++ ideaJava-jdk8/src/com/comanche/temp/test.java
@@ -4,6 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("hot_fix");
+        System.out.println("测试");
     }
 
 }
