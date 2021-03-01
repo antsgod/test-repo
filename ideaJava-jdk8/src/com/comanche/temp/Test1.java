896e981651438a17655938a6eda0556287e2193e
antsgod <antsgod30329398@163.com>
2021-03-02 00:11:00 +0800

    测试合并分支

--- ideaJava-jdk8/src/com/comanche/temp/Test1.java
+++ ideaJava-jdk8/src/com/comanche/temp/Test1.java
@@ -3,5 +3,6 @@ package com.comanche.temp;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("测试");
+        System.out.println("host_fix分支修改");
     }
 }
